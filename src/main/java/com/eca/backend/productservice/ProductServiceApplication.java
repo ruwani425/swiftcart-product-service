a1d948b47b273e5c21b7e4688083b8da8e497c2b
Ruwani Ranthika <ruwanielv@gmail.com>
2026-08-27 22:51:36 +0530

    fix: clean ProductServiceApplication main class

--- src/main/java/com/eca/backend/productservice/ProductServiceApplication.java
+++ src/main/java/com/eca/backend/productservice/ProductServiceApplication.java
@@ -3,16 +3,9 @@ package com.eca.backend.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@ComponentScan(basePackages = {
-    "com.eca.backend.productservice.controller",
-    "com.eca.backend.productservice.service",
-    "com.eca.backend.productservice.repository",
-    "com.eca.backend.productservice.config"
-})
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
